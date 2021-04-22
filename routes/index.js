aa071fd4a322a6b9b48ab5cc665c1a9a66680f87
amanninen <amanninen76@gmail.com>
2021-04-22 23:33:36 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ const pool = new Pool({
 router.get('/', function(req, res, next) {
   console.log('routing again index');
 
-  pool.query(`INSERT INTO salesforce.lead(FirstName,LastName,Company)VALUES($1,$2,$3)`, 
+  pool.query(`INSERT INTO salesforce.lead(FirstName,LastName,Company)VALUES($1,$2,$3);`, 
             ['Ari-Pekka','Manninen','Invisaling'], (err, res) => {
     if (err) {
         console.log("Error - Failed to insert data into ccLeads");
@@ -29,7 +29,7 @@ router.get('/', function(req, res, next) {
 router.post('/leadform', async function (req, res, next) {
   console.log('routing on post async form');
   console.log(req.body);
-  const result = await pool.query(`insert into salesforce.lead(firstname, lastname, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
+  const result = await pool.query(`insert into salesforce.lead(firstname, lastname, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign');`);
   /*await pool.query(`INSERT INTO salesforce.lead(FirstName,LastName,Company)VALUES($1,$2,$3)`, 
             ['FirstName','LastName','Invisaling'], (err, res) => {
     if (err) {
