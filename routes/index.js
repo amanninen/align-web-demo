4892ff02de344f6b8bc6668a5f7cc252abfffebf
amanninen <amanninen76@gmail.com>
2021-04-22 22:29:13 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -14,18 +14,7 @@ const pool = new Pool({
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('routing in once again index');
-  /*console.log(pool);
-  pool.query('SELECT * FROM salesforce.lead;', (err, res) => {
-    if (err) {
-        console.log("Error - Failed to select all from leads");
-        console.log(err);
-    }
-    else{
-        console.log('OU JEAH SOMETHING');
-        console.log(res.rows);
-    }
-  }); 
-  */
+
   res.render('locator', {success: false});
 });
 
@@ -34,12 +23,13 @@ router.post('/leadform', async function (req, res, next) {
   console.log('routing on post form with updated code');
   console.log(req.body);
   //const result = await pool.query(`insert into salesforce.lead(firstname, lastname, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
-  pool.query(`INSERT INTO salesforce.lead(firstname, lastname, mobilephone, postalcode, date_of_birth__c, email, company) VALUES ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`, (err, res) => {
+  pool.query(`INSERT INTO salesforce.lead(firstname, lastname, mobilephone, postalcode, date_of_birth__c, email, company) 
+        VALUES ($1,$2$,$3,$4,$5,$6,$7)`, ['${req.body.txtFirstName}', '${req.body.txtLastName}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign'], (err, res2) => {
     if (err) {
         console.log("Error - Failed to insert data into leads");
         console.log(err);
     }
-  console.log(res);
+  console.log(res2);
   res.render('locator', { success: true });
 });
 
