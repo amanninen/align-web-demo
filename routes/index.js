9c00e658f11090a6a7c09569452a81481e7dcd2c
amanninen <amanninen76@gmail.com>
2021-04-22 22:33:06 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -23,12 +23,13 @@ router.post('/leadform', async function (req, res, next) {
   console.log('routing on post form with updated code');
   console.log(req.body);
   //const result = await pool.query(`insert into salesforce.lead(firstname, lastname, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
-  pool.query(`INSERT INTO salesforce.lead(firstname, lastname, mobilephone, postalcode, date_of_birth__c, email, company) 
-        VALUES ($1,$2$,$3,$4,$5,$6,$7)`, ['${req.body.txtFirstName}', '${req.body.txtLastName}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign'], (err, res2) => {
+  pool.query(`INSERT INTO salesforce.lead(firstname, lastname, company) 
+        VALUES ($1,$2$,$3)`, ['${req.body.txtFirstName}', '${req.body.txtLastName}','Invisalign'], (err, res2) => {
     if (err) {
         console.log("Error - Failed to insert data into leads");
         console.log(err);
     }
+  });
   console.log(res2);
   res.render('locator', { success: true });
 });
