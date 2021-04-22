11cf059150235e375523a4fb8946dc56b4e3f198
amanninen <amanninen76@gmail.com>
2021-04-22 23:26:26 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -29,15 +29,16 @@ router.get('/', function(req, res, next) {
 router.post('/leadform', async function (req, res, next) {
   console.log('routing on post async form');
   console.log(req.body);
-  //const result = await pool.query(`insert into salesforce.lead(firstname, lastname, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
-  await pool.query(`INSERT INTO salesforce.lead(FirstName,LastName,Company)VALUES($1,$2,$3)`, 
+  const result = await pool.query(`insert into salesforce.lead(firstname, lastname, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
+  /*await pool.query(`INSERT INTO salesforce.lead(FirstName,LastName,Company)VALUES($1,$2,$3)`, 
             ['FirstName','LastName','Invisaling'], (err, res) => {
     if (err) {
         console.log("Error - Failed to insert data into Leads");
         console.log(err);
     }
   });
-  
+  */
+  console.log("what we got:"+result);
   res.render('locator', { success: true });
 });
 
