8ecc60f34e9fd15d2f854d4ba8176936f4a316cb
amanninen <amanninen76@gmail.com>
2021-04-22 21:36:39 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -15,12 +15,13 @@ const pool = new Pool({
 router.get('/', function(req, res, next) {
   console.log('routing in once again index');
   console.log(pool);
-  pool.query(`SELECT * FROM salesforce.lead;`, (err, res) => {
+  pool.query('SELECT * FROM salesforce.lead;', (err, res) => {
     if (err) {
         console.log("Error - Failed to select all from leads");
         console.log(err);
     }
     else{
+        console.log('OU JEAH SOMETHING');
         console.log(res.rows);
     }
   }); 
@@ -32,7 +33,7 @@ router.get('/', function(req, res, next) {
 router.post('/leadform', async function (req, res, next) {
   console.log('routing on post form f');
   console.log(req.body);
-  const result = await pool.query(`insert into salesforce.lead(firstname, lastname, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
+  const result = pool.query(`insert into salesforce.lead(firstname, lastname, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
   console.log(result);
   res.render('locator', { success: true });
 });
