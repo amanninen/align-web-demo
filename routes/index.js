26f13866e9c7392253ed5f93d05315726f9b10e8
amanninen <amanninen76@gmail.com>
2021-04-22 21:05:15 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -11,22 +11,13 @@ const pool = new Pool({
   }
 });
 
-/*const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-*/
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('routing in again index');
+  console.log('routing in once again index');
+  console.log(pool);
   pool.query(`SELECT * FROM salesforce.lead;`, (err, res) => {
     if (err) {
-        console.log("Error - Failed to select all from Users");
+        console.log("Error - Failed to select all from leads");
         console.log(err);
     }
     else{
@@ -39,7 +30,7 @@ router.get('/', function(req, res, next) {
 
 /* form handling */
 router.post('/leadform', async function (req, res, next) {
-  console.log('routing on post form');
+  console.log('routing on post form f');
   console.log(req.body);
   const result = await pool.query(`insert into salesforce.lead(firstname, lastname, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
   console.log(result);
